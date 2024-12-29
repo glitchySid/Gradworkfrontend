f8c8dc5deec646b96b6415fdd4202efd8c5a3eb1
glitchySid <siddheshmhatre2005@gmail.com>
2024-12-29 12:49:30 +0530

    added new profilesetuppage at /setupprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegistrationPage from "./pages/registration.jsx";
 import GradWorkLanding from "./pages/homepage.jsx";
+import ProfileSetup from "./pages/profilesetup_inc.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<GradWorkLanding />} />
           <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/setupprofile" element={<ProfileSetup />} />
+          <Route path="*" element={<GradWorkLanding />} />
         </Routes>
       </div>
     </Router>
