3f4f89baeb6ab42c1932cb87685e54c235406f26
glitchySid <siddheshmhatre2005@gmail.com>
2025-01-20 19:39:28 +0530

    changed font to monserrat

--- src/components/ui/hero.jsx
+++ src/components/ui/hero.jsx
@@ -5,13 +5,14 @@ const Hero = () => (
   <div className="relative h-96 bg-gray-100 flex items-center justify-center">
     <div className="absolute inset-0">
       <img
-        src={heroImage}
+        src={heroImage} 
         alt="Student studying"
-        className="w-full h-full object-cover opacity-60 brightness-75"
+        className="w-full h-full object-cover opacity-50 backdrop-brightness-110"
       />
+      <div className="absolute inset-0 bg-black opacity-70"></div>
     </div>
     <div className="relative z-10 text-center px-3">
-      <h1 className="text-3xl font-bold mb-6">Empowering College Students</h1>
+      <h1 className="text-2xl font-semibold text-white mb-6 font-MNS">Empowering College Students</h1>
       <SearchBar />
     </div>
   </div>
